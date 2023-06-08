396bb1a2b80c543992e1679316c4107224da4868
opsrcode <opsrcode@hotmail.com>
2023-06-08 14:47:36 -0300

    Compilação '-m32' GCC

--- instalar-programas.sh
+++ instalar-programas.sh
@@ -10,24 +10,25 @@
 sudo pacman -Syyuu
 sudo pacman -S man man-pages texinfo qutebrowser firefox ufw ctags cscope\
 	cmake fzf git nasm gdb bash-completion ttf-dejavu-nerd dex xss-lock\
-	notification-daemon brightnessctl powertop tlp tlp-rdw p7zip
+	notification-daemon brightnessctl powertop tlp tlp-rdw p7zip lib32-libltdl
 
 # Configuração ufw
-sudo ufw default deny incoming; sudo systemctl enable ufw; sudo systemctl start ufw; sudo ufw enable
+sudo ufw default deny incoming; sudo systemctl enable ufw
+sudo systemctl start ufw; sudo ufw enable
 
 # Configuração TLP services
 sudo systemctl enable tlp.service && sudo systemctl start tlp.service
-sudo systemctl mask systemd-rfkill.service && sudo systemctl mask systemd-rfkill.socket
+sudo systemctl mask systemd-rfkill.service
+sudo systemctl mask systemd-rfkill.socket
 
 # YAY, instalação e configuração inicial
-cd /tmp; git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si; cd ..
-yay -Y --gendb; yay -Syu --devel; yay -Y --devel --save
+cd /tmp; git clone https://aur.archlinux.org/yay.git; cd yay;
+makepkg -si; cd ..; yay -Y --gendb; yay -Syu --devel; yay -Y --devel --save
 yay sioyek chromium-widevine vi
 
 # auto-cpufreq, instalação e configuração inicial
-git clone https://github.com/AdnanHodzic/auto-cpufreq.git
-cd auto-cpufreq && sudo ./auto-cpufreq-installer; cd ..
-sudo auto-cpufreq --install
+git clone https://github.com/AdnanHodzic/auto-cpufreq.git; cd auto-cpufreq
+sudo ./auto-cpufreq-installer; cd ..; sudo auto-cpufreq --install
 
 # LaTeX, compilação e configurações de variáveis (PATH, MANPATH e INFOPATH)
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
