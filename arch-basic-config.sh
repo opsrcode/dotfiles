c9b257403ee1d1d836a402d4609b83d2b6a03c0c
srcbug <devtces@hotmail.com>
2023-12-27 22:00:45 -0300

    Few changes :]

--- arch-basic-config.sh
+++ arch-basic-config.sh
@@ -18,6 +18,9 @@ sudo pacman -S base-devel network-manager-applet xss-lock xdg-utils dex \
                notification-daemon libnotify bash-completion brightnessctl \
                rxvt-unicode powertop ufw p7zip man man-pages texinfo \
                ttf-dejavu-nerd qutebrowser mupdf fzf
+
+# sudo pacman -S gdb valgrind splint php nodejs xampp jdk-openjdk
+ 
 printf "===Download=e=Instalação=VIM=============>>\n"
 sudo pacman -R vim nano
 cd /tmp ; git clone https://github.com/vim/vim.git ; cd /tmp/vim
@@ -30,20 +33,21 @@ make && sudo make install ; cd /tmp ; sudo mv vim /usr/local/src/
 printf "===Download=e=Instalação=YAY=============>>\n"
 cd /tmp ; git clone https://aur.archlinux.org/yay.git ; cd /tmp/yay ; makepkg -si
 cd /tmp ; rm -rf yay ; yay -Y --gendb ; yay -Syu --devel ; yay -Y --devel --save
-yay -S chromium-widevine
 printf "===Download=e=Instalação=Auto=CPU=Freq===>>\n"
 cd /tmp ; git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq ; sudo ./auto-cpufreq-installer ; cd /tmp ; rm -rf auto-cpufreq
-printf "===Download=e=Instalação=TeX=Live========>>\n"
-cd /tmp ; wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-zcat < install-tl-unx.tar.gz | tar xf - ; cd install-tl-2*
-sudo perl ./install-tl --no-interaction ; cd /tmp ; rm -rf install-tl-*
-printf "===Download=e=Instalação=MariaDB=========>>\n"
-sudo pacman -S mariadb
-sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-sudo systemctl start mariadb ; sudo mysql_secure_installation 
-printf "\n\nConnects to the MariaDB Server on the 'localhost':\n\t\
-mariadb -h [hostname] -u [username] -p[password] [database_name]\n\n"
+
+# printf "===Download=e=Instalação=TeX=Live========>>\n"
+# cd /tmp ; wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+# zcat < install-tl-unx.tar.gz | tar xf - ; cd install-tl-2*
+# sudo perl ./install-tl --no-interaction ; cd /tmp ; rm -rf install-tl-*
+# printf "===Download=e=Instalação=MariaDB=========>>\n"
+# sudo pacman -S mariadb
+# sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+# sudo systemctl start mariadb ; sudo mysql_secure_installation 
+# printf "\n\nConnects to the MariaDB Server on the 'localhost':\n\t\
+# mariadb -h [hostname] -u [username] -p[password] [database_name]\n\n"
+ 
 printf "===Configurações=Firewall=e=Bateria======>>\n"
 sudo ufw default deny incoming ; sudo systemctl enable ufw ; sudo ufw enable 
 sudo ufw status verbose
