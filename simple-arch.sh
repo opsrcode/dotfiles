3ca70819fedfad2f3ba7de58ef76f5b98134d8a0
myerrors <professional-dev@hotmail.com>
2024-02-14 03:14:02 -0300

    New Script and .bash_profile configurations

--- simple-arch.sh
+++ simple-arch.sh
@@ -1,27 +1,36 @@
 #!/bin/sh
 
-printf "===Arquivos=de=configuração==============>>\n"
-mv .bash* .Xresources .vim .vimrc ~
-mv .config/i3/config ~/.config/i3
-printf "===Reparos=Instalação=e=Touchpad=========>>\n"
-printf "Gerando arquivo de configuração do touchpad\n"
+function section_title (
+  function border {
+    printf " ~~~~~%s\n" $(for i in $@ ; do for j in $(seq 1 ${#i}) ; do printf '~'
+                          done ; printf '~' ; done)
+  } ; border $@ ; echo -e '~~~ '$@' ~~~' ; border $@ ; printf "\n"
+)
+
+
+section_title 'Arquivos de configuração'
+mv .bash* .Xresources .vim .vimrc ~ ; mv .config/i3/config ~/.config/i3
+ 
+ 
+section_title 'Configurações de Notificação e Touchpad'
 sudo sh -c 'printf "Section \"InputClass\"\n\tIdentifier \"touchpad\"\n\
-\tMatchIsTouchpad \"on\"\n\tDriver \"libinput\"\n\
-\tOption \"Tapping\" \"on\"\nEndSection" > /etc/X11/xorg.conf.d/90-touchpad.conf'
-printf "Gerando arquivo de configuração das notificações\n"
+\tMatchIsTouchpad \"on\"\n\tDriver \"libinput\"\n\tOption \"Tapping\" \"on\"\n\
+EndSection" > /etc/X11/xorg.conf.d/90-touchpad.conf'
+ 
 sudo sh -c 'printf "[D-BUS Service]\nName=org.freedesktop.Notifications\n\
 Exec=/usr/lib/notification-daemon-1.0/notification-daemon" > \
 /usr/share/dbus-1/services/org.freedesktop.Notifications.service'
-printf "===Download=Softwares=Essenciais=========>>\n"
-sudo pacman -Syyuu
-sudo pacman -S base-devel network-manager-applet xss-lock xdg-utils dex \
-               notification-daemon libnotify bash-completion brightnessctl \
-               rxvt-unicode powertop ufw man man-pages texinfo
-
-# sudo pacman -S gdb valgrind splint php nodejs jdk-openjdk
  
-printf "===Download=e=Instalação=VIM=============>>\n"
-sudo pacman -R vim nano
+ 
+section_title 'Software Essenciais'
+sudo pacman --noconfirm -Syyuu
+sudo pacman --noconfirm -S base-devel network-manager-applet xss-lock dex gdb \
+xdg-utils notification-daemon libnotify bash-completion brightnessctl mupdf \
+rxvt-unicode powertop ufw man man-pages texinfo valgrind splint
+   
+
+section_title 'Compilação VIM'
+sudo pacman --noconfirm -R vim nano
 cd /tmp ; git clone https://github.com/vim/vim.git ; cd /tmp/vim
 ./configure --with-features=huge --enable-multibyte --enable-python3interp=yes \
             --with-python3-command=$PYTHON_VER \
@@ -29,30 +38,37 @@ cd /tmp ; git clone https://github.com/vim/vim.git ; cd /tmp/vim
             --enable-perlinterp=yes --enable-luainterp=yes \
             --with-lua-prefix=/usr/local --enable-gui=gtk2 --prefix=/usr/local
 make && sudo make install ; cd /tmp ; sudo mv vim /usr/local/src/
-printf "===Download=e=Instalação=YAY=============>>\n"
+   
+
+section_title 'Compilação YAY (Gerenciador de pacotes AUR)'
 cd /tmp ; git clone https://aur.archlinux.org/yay.git ; cd /tmp/yay ; makepkg -si
 cd /tmp ; rm -rf yay ; yay -Y --gendb ; yay -Syu --devel ; yay -Y --devel --save
-yay -S ttf-iosevka google-chrome
-
-# yay -S xampp
-
-printf "===Download=e=Instalação=Auto=CPU=Freq===>>\n"
+yay --noconfirm --diff-noconfirm -S ttf-iosevka google-chrome
+ 
+   
+section_title 'Compilação Auto CPU Freq'
 cd /tmp ; git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq ; sudo ./auto-cpufreq-installer ; cd /tmp ; rm -rf auto-cpufreq
-
-# printf "===Download=e=Instalação=TeX=Live========>>\n"
+ 
+   
+# section_title 'Compilação TeX Live'
 # cd /tmp ; wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 # zcat < install-tl-unx.tar.gz | tar xf - ; cd install-tl-2*
 # sudo perl ./install-tl --no-interaction ; cd /tmp ; rm -rf install-tl-*
-# printf "===Download=e=Instalação=MariaDB=========>>\n"
-# sudo pacman -S mariadb
+# echo -e 'Descomentar variáveis de sistema em ~/.bash_profile'
+
+
+# section_title 'Instalação e Configuração MariaDB'
+# sudo pacman --noconfirm -S mariadb
 # sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 # sudo systemctl start mariadb ; sudo mysql_secure_installation 
 # printf "\n\nConnects to the MariaDB Server on the 'localhost':\n\t\
 # mariadb -h [hostname] -u [username] -p[password] [database_name]\n\n"
  
-printf "===Configurações=Firewall=e=Bateria======>>\n"
+   
+section_title 'Configuração UFW, Powertop e Auto CPU Freq'
 sudo ufw default deny incoming ; sudo systemctl enable ufw ; sudo ufw enable 
-sudo ufw status verbose
-sudo powertop --calibrate ; sudo powertop --auto-tune
+sudo ufw status verbose ; sudo powertop --calibrate ; sudo powertop --auto-tune
 sudo auto-cpufreq --install
+
+section_title 'Configuração de setup Arch Linux básico finalizado com sucesso!'
