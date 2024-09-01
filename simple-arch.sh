2ed131b8b00ef54195973ac30263f167b373baf3
Thiago Castro <141454158+opsrcode@users.noreply.github.com>
2024-08-31 22:55:36 -0300

    Update simple-arch.sh

--- simple-arch.sh
+++ simple-arch.sh
@@ -23,12 +23,7 @@ section_title 'Software Essenciais'
 sudo pacman --noconfirm -Syyuu
 sudo pacman --noconfirm -S base-devel network-manager-applet xss-lock dex \
 xdg-utils notification-daemon libnotify bash-completion brightnessctl \
-rxvt-unicode powertop ufw man man-pages texinfo ttc-iosevka irssi mupdf
-
-section_title 'Compilação Ungoogled Chromium'
-cd ~ ; git clone https://github.com/ungoogled-software/ungoogled-chromium-archlinux
-cd ungoogled-chromium-archlinux ; git checkout $(git describe --abbrev=0 --tags)
-makepkg -s ; makepkg --install ; cd ~ ; rm -rf ungoogled-chromium-archlinux
+rxvt-unicode powertop ufw man man-pages texinfo ttc-iosevka
 
 section_title 'Compilação VIM'
 sudo pacman --noconfirm -R vim nano
@@ -39,29 +34,15 @@ cd /tmp ; git clone https://github.com/vim/vim.git ; cd ./vim
             --enable-perlinterp=yes --enable-luainterp=yes \
             --with-lua-prefix=/usr/local --enable-gui=gtk2 --prefix=/usr/local
 make && sudo make install ; cd /tmp ; sudo mv vim /usr/local/src/
-# sudo pacman --noconfirm -S ctags ; mkdir -p ~/.vim/pack/downloads/start
-# git clone https://github.com/yegappan/taglist.git
-# mv ./taglist ~/.vim/pack/downloads/start
 
 section_title 'Compilação YAY (Gerenciador de pacotes AUR)'
 git clone https://aur.archlinux.org/yay.git ; cd /tmp/yay ; makepkg -si
 cd /tmp ; rm -rf yay ; yay -Y --gendb ; yay -Syu --devel ; yay -Y --devel --save
+yay -S ungoogled-chromium-bin
    
 section_title 'Compilação Auto CPU Freq'
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq ; sudo ./auto-cpufreq-installer ; cd /tmp ; rm -rf auto-cpufreq
-
-# section_title 'Compilação TeX Live'
-# wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-# zcat < install-tl-unx.tar.gz | tar xf - ; cd install-tl-2*
-# sudo perl ./install-tl --no-interaction ; cd /tmp ; rm -rf install-tl-*
-
-# section_title 'Instalação e Configuração MariaDB'
-# sudo pacman --noconfirm -S mariadb
-# sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-# sudo systemctl start mariadb ; sudo mysql_secure_installation 
-# printf "\n\nConnects to the MariaDB Server on the 'localhost':\n\t\
-# mariadb -h [hostname] -u [username] -p[password] [database_name]\n\n"
  
 section_title 'Configuração UFW, Powertop e Auto CPU Freq'
 sudo ufw default deny incoming ; sudo systemctl enable ufw ; sudo ufw enable 
