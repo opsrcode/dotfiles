fde3491a94d7fc1b7139eb963e1865eb32581f56
Thiago Castro <141454158+opsrcode@users.noreply.github.com>
2025-03-11 18:03:23 -0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,52 +1,41 @@
 #!/bin/sh
 
-set -xoeu pipefail
-
-function def_vars
-{
-	repo=$(pwd)
-
-	based='/dev/sda'
-	bootd=$based"1"
-	swapd=$based"2"
-	rootd=$based"3"
-	pac="/etc/pacman.d"
-
-	font='ter-114n'
-	keymap='us'
-	timezone='America/Sao_Paulo'
-	user='opsrcode'
-	shell='/usr/bin/bash'
-	pkgs="$(cat <<EOF
+repo=$(pwd)
+based='/dev/sda'
+bootd=$based"1"
+swapd=$based"2"
+rootd=$based"3"
+pac="/etc/pacman.d"
+font='ter-114n'
+keymap='us'
+timezone='America/Sao_Paulo'
+user='opsrcode'
+shell='/usr/bin/bash'
+pkgs="$(cat <<EOF
 base base-devel linux linux-firmware xf86-video-amdgpu xf86-video-ati
 amd-ucode dhclient openssh ufw powertop mesa vulkan-radeon polkit git
 libx11 libxinerama libxft freetype2 xorg-xinit xorg-server 
 terminus-font man-db man-pages texinfo
 EOF
 )"
-
-	boot="/mnt/boot"
-	etc="/mnt/etc"
-	home="/mnt/home/$user"
-}
-
-function def_ctts
-{
-	vconsole="$(cat <<EOF
+boot="/mnt/boot"
+etc="/mnt/etc"
+home="/mnt/home/$user"
+vconsole="$(cat <<EOF
 KEYMAP=us
 FONT=ter-114n
 EOF
 )"
-	locale='LANG=en_US.UTF-8'
-	hostname='archlinux'
-	hosts="$(cat <<EOF
+locale='LANG=en_US.UTF-8'
+hostname='archlinux'
+hosts="$(cat <<EOF
 127.0.0.1	localhost
 ::1		localhost ip6-localhost ip6-loopback
 EOF
 )"
-	sudoers="$user ALL=(ALL:ALL) ALL"
-	local pid="$(blkid $rootd | awk -F'PARTUUID=' '{print $2}' | sed 's/"//g')"
-	arch="$(cat <<EOF
+sudoers="$user ALL=(ALL:ALL) ALL"
+local pid="$(blkid $rootd | awk -F'PARTUUID=' '{print $2}' | sed 's/"//g')"
+arch="$(cat <<EOF
 title	Arch Linux
 linux	/vmlinuz-linux
 initrd	/amd-ucode.img
@@ -54,12 +43,12 @@ initrd	/initramfs-linux.img
 options	root=PARTUUID=$pid rootfstype=ext4 rw
 EOF
 )"
-	loader="$(cat <<EOF
+loader="$(cat <<EOF
 default arch.conf
 console-mode auto
 EOF
 )"
-	brc="$(cat <<EOF
+brc="$(cat <<EOF
 [[ \$- != *i* ]] && return
 PS1='[\u@\h \W]\$ '
 VISUAL='/usr/bin/vim'
@@ -67,14 +56,20 @@ export EDITOR="\$VISUAL"
 set -o vi
 EOF
 )"
-	bprc="$(cat <<EOF
+bprc="$(cat <<EOF
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 if [ -z "\$DISPLAY" ] && [ "\$XDG_VTNR" = 1 ]; then
 	exec startx
 fi
 EOF
 )"
-}
+part="$(cat <<EOF
+label: gpt
+size=1GiB, type=U, name="EFI System Partition"
+size=10GiB, type=S, name="Linux Swap Partition"
+size=+, type=L, name="Linux Filesystem Partition"
+EOF
+)"
 
 function basics
 {
@@ -85,17 +80,10 @@ function basics
 function partition
 {
 	sfdisk --delete "$based" && wipefs -a "$based" && \
-	sfdisk "$based" <<EOF
-label: gpt
-size=1GiB, type=U, name="EFI System Partition"
-size=10GiB, type=S, name="Linux Swap Partition"
-size=+, type=L, name="Linux Filesystem Partition"
-EOF
+	sfdisk "$based" "$part"
 	mkfs.fat -F32 "$bootd" && mkfs.ext4 "$rootd" && \
 	mkswap "$swapd" && mount "$rootd" /mnt && \
 	mount --mkdir "$bootd" "$boot" && swapon "$swapd"
-
-	lsblk
 }
 
 function load_pkgs
@@ -123,25 +111,23 @@ function change_files
 
 function chroot_config
 {
-	local pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git')
 	local mhome="/home/$user"
+	local pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git')
 
-	cd "$mhome" && for pkg in "${pkgs[@]}"; do
+	cd "$home" && for pkg in "${pkgs[@]}"; do
 		mkdir $pkg && cp "$repo/$pkg" "$pkg/PKGBUILD"
-       	done && cd "$repo" && cp -r .vim* "$mhome"
+       	done && cd "$repo" && cp -r .vim* "$home"
 
 	arch-chroot /mnt "$shell" -c "$(cat <<EOF
 passwd && ln -sf "/usr/share/zoneinfo/$timezone" /etc/localtime
 hwclock --systohc && locale-gen && useradd -m -g users -s "$shell" "$user"
-passwd "$user" && pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git')
-pacman -Syyuu && chown -R "$user":users "$mhome" && cd "$mhome"
+passwd "$user" && pacman -Syyuu && chown -R "$user":users "$mhome" && cd "$mhome" 
+pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git') && \
 for pkg in \${pkgs[@]} ; do cd \$pkg && runuser -u "$user" -- makepkg -sri
-cd "$mhome" && mv \$pkg /usr/src/ && ; done
-cd "$mhome" && printf "$brc" > .bashrc && printf "$bprc" > .bash_profile
-git clone https://aur.archlinux.org/yay.git && chown -R "$user:users" yay
-cd yay && runuser -u "$user" -- makepkg -sri
-runuser -u "$user" -- yay -Y --gendb
-runuser -u "$user" -- yay -Syu --devel
+cd "$mhome" && mv \$pkg /usr/src/ ; done && printf "$brc" > .bashrc && \
+printf "$bprc" > .bash_profile && git clone https://aur.archlinux.org/yay.git && \
+chown -R "$user:users" yay && cd yay && runuser -u "$user" -- makepkg -sri
+runuser -u "$user" -- yay -Y --gendb && runuser -u "$user" -- yay -Syu --devel
 runuser -u "$user" -- yay -Y --devel --save && cd "$mhome"
 rm -rf yay && bootctl install
 EOF
@@ -158,7 +144,6 @@ function boot_config
 
 function main
 {
-	def_vars && def_ctts && basics && partition && load_pkgs && \
-	change_files && chroot_config && boot_config
+	basics && partition && load_pkgs && change_files && chroot_config && boot_config
 }
 main
