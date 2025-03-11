991dd2def8dd196472951c4f30f3bc93c15c5d3d
Thiago Castro <141454158+opsrcode@users.noreply.github.com>
2025-03-11 17:14:17 -0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -euo pipefail
+set -xoeu pipefail
 
 function def_vars
 {
@@ -10,6 +10,7 @@ function def_vars
 	bootd=$based"1"
 	swapd=$based"2"
 	rootd=$based"3"
+	pac="/etc/pacman.d"
 
 	font='ter-114n'
 	keymap='us'
@@ -26,14 +27,18 @@ EOF
 
 	boot="/mnt/boot"
 	etc="/mnt/etc"
-	pac="$etc/pacman.d"
 	home="/mnt/home/$user"
 }
 
 function def_ctts
 {
-	vconsole="KEYMAP=us\nFONT=ter-114n"
+	vconsole="$(cat <<EOF
+KEYMAP=us
+FONT=ter-114n
+EOF
+)"
 	locale='LANG=en_US.UTF-8'
+	hostname='archlinux'
 	hosts="$(cat <<EOF
 127.0.0.1	localhost
 ::1		localhost ip6-localhost ip6-loopback
@@ -55,14 +60,16 @@ console-mode auto
 EOF
 )"
 	brc="$(cat <<EOF
+[[ \$- != *i* ]] && return
+PS1='[\u@\h \W]\$ '
 VISUAL='/usr/bin/vim'
-export EDITOR=\"\$VISUAL\"
+export EDITOR="\$VISUAL"
 set -o vi
 EOF
 )"
 	bprc="$(cat <<EOF
 [[ -f ~/.bashrc ]] && . ~/.bashrc
-if [ -z \"\$DISPLAY\" ] && [ \"\$XDG_VTNR\" = 1 ]; then
+if [ -z "\$DISPLAY" ] && [ "\$XDG_VTNR" = 1 ]; then
 	exec startx
 fi
 EOF
@@ -71,7 +78,7 @@ EOF
 
 function basics
 {
-	loadkeys "$keymap" && set font "$font" && \
+	loadkeys "$keymap" && setfont "$font" && \
 	timedatectl set-timezone "$timezone" && timedatectl set-ntp true 
 }
 
@@ -87,28 +94,29 @@ EOF
 	mkfs.fat -F32 "$bootd" && mkfs.ext4 "$rootd" && \
 	mkswap "$swapd" && mount "$rootd" /mnt && \
 	mount --mkdir "$bootd" "$boot" && swapon "$swapd"
+
+	lsblk
 }
 
 function load_pkgs
 {
 	reflector --latest 20 --sort rate --country Brazil --save "$pac/mirrorlist" && \
-	pacstrap -K /mnt "$pkgs" && genfstab -U mnt >> "$etc/fstab"
+	pacstrap -K /mnt $pkgs && genfstab -U /mnt >> $etc/fstab
 }
 
 function change_files
 {
-	local xi='./.xinitrc'
+	local xi='.xinitrc'
 	printf "$vconsole" > "$etc/vconsole.conf"
-	sed -i '/en_US.UTF-8/s/^# //' "$etc/locale.gen"
+	sed -i '/en_US.UTF-8/s/^#//' "$etc/locale.gen"
 	printf "$locale" > "$etc/locale.conf"
-	printf "$arch" > "$etc/hostname"
+	printf "$hostname" > "$etc/hostname"
 	printf "$hosts" >> "$etc/hosts"
 	printf "$sudoers" >> "$etc/sudoers"
 	sed -i -e '/^#VerbosePkgLists/s/^#//' \
  	       -e '/^#\[multilib\]/,/^#Include/s/^#//' "$etc/pacman.conf"
 	sed -i -e '/^MODULES=()/s/()/(amd radeon)/' "$etc/mkinitcpio.conf"
-	mkdir "$home" && cd "$home" &&
-	cp "$etc/X11/xinit/xinitrc" "$xi" && \
+	mkdir "$home" && cd "$home" && cp "$etc/X11/xinit/xinitrc" "$xi" && \
 	sed -i '/\$twm \&/,$d' "$xi" && echo 'exec dwm' >> "$xi"
 	cd $repo
 }
@@ -116,22 +124,28 @@ function change_files
 function chroot_config
 {
 	local pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git')
+	local mhome="/home/$user"
 
-	cd "$home" && for pkg in "${pkgs[@]}"; do
-		mkdir $pkg && mv "$repo/$pkg" "$pkg/PKGBUILD"
-       	done && cd "$repo" && cp ".vim*" "$home"
+	cd "$mhome" && for pkg in "${pkgs[@]}"; do
+		mkdir $pkg && cp "$repo/$pkg" "$pkg/PKGBUILD"
+       	done && cd "$repo" && cp -r .vim* "$mhome"
 
-	arch-chroot mnt && passwd
-	ln -sf "/usr/share/zoneinfo/$timezone" /etc/localtime
-	hwclock --systohc && locale-gen
-
-	useradd -m -g users -s "$shell" "$user" && passwd "$user" && su opsrcode
-	for pkg in "${pkgs[@]}"; do cd "~/$pkg" && makepkg -sri ; done && cd /tmp
-	printf "$brc" >> "~/.bashrc" && printf "$bprc" >> "~/.bash_profile"
-	git clone https://aur.archlinux.org/yay.git && cd ./yay && makepkg -sri 
-	yay -Y --gendb && yay -Syu --devel && yay -Y --devel --save && exit
-
-       	bootctl install && exit
+	arch-chroot /mnt "$shell" -c "$(cat <<EOF
+passwd && ln -sf "/usr/share/zoneinfo/$timezone" /etc/localtime
+hwclock --systohc && locale-gen && useradd -m -g users -s "$shell" "$user"
+passwd "$user" && pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git')
+pacman -Syyuu && chown -R "$user":users "$mhome" && cd "$mhome"
+for pkg in \${pkgs[@]} ; do cd \$pkg && runuser -u "$user" -- makepkg -sri
+cd "$mhome" && mv \$pkg /usr/src/ && ; done
+cd "$mhome" && printf "$brc" > .bashrc && printf "$bprc" > .bash_profile
+git clone https://aur.archlinux.org/yay.git && chown -R "$user:users" yay
+cd yay && runuser -u "$user" -- makepkg -sri
+runuser -u "$user" -- yay -Y --gendb
+runuser -u "$user" -- yay -Syu --devel
+runuser -u "$user" -- yay -Y --devel --save && cd "$mhome"
+rm -rf yay && bootctl install
+EOF
+)"
 }
 
 function boot_config
