0b7e3c9d52daf1b12dedb1fa581b5bd417628900
Thiago Castro <141454158+opsrcode@users.noreply.github.com>
2025-04-03 14:36:42 -0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -103,7 +103,7 @@ function change_files
 function chroot_config
 {
 	local mhome="/home/$user"
-	local pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git' 'w3m-git' 'tor-git' 'irssi-git')
+	local pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git' 'w3m-git' 'tor-git' 'irssi-git' 'torsocks-git')
 
 	cd "$home" && for pkg in "${pkgs[@]}"; do
 		mkdir $pkg && cp "$repo/$pkg" "$pkg/PKGBUILD"
@@ -113,7 +113,7 @@ function chroot_config
 passwd && ln -sf "/usr/share/zoneinfo/$timezone" /etc/localtime
 hwclock --systohc && locale-gen && useradd -m -g users -s "$shell" "$user"
 passwd "$user" && pacman -Syyuu && chown -R "$user":users "$mhome" && cd "$mhome" 
-pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git' 'w3m-git' 'tor-git' 'irssi-git') && \
+pkgs=('vim-git' 'dwm-git' 'st-git' 'dmenu-git' 'w3m-git' 'tor-git' 'irssi-git' 'torsocks-git') && \
 for pkg in \${pkgs[@]} ; do cd \$pkg && runuser -u "$user" -- makepkg -sri
 cd "$mhome" && mv \$pkg /usr/src/ ; done && printf "$brc" > .bashrc && \
 printf "$bprc" > .bash_profile && bootctl install
